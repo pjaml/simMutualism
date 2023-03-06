a355c91d24bbfa405f2459abbe5c301a155b61b4
pjaml <lutzx119@umn.edu>
2023-03-06 16:24:36 -0600

    added parfor to parameter sweep script

--- tauSweep.m
+++ tauSweep.m
@@ -1,12 +1,24 @@
+cl=parcluster('local')
+pool = cl.parpool(16)
+
+% use integers for the number of iterations to run
+% since parfor requires it
+rangeStartInt = 0
+rangeInt = 40
+rangeStep = 0.01
 iterations = 100;
 maxIterations = 1000;
-[tau12Range, tau21Range] = deal(0:0.01:0.4);
-outputDir = './tauSweep/';
+[tau12Range, tau21Range] = deal(rangeStartInt:rangeStartInt);
 
-for tau12 = tau12Range
+outputDir = '~/tauSweep/';
 
-    for tau21 = tau21Range
+parfor i = tau12Range
 
+    tau12 = i * rangeStep;
+
+    for j = tau21Range
+
+        tau21 = j * rangeStep;
         % more iterations for tau values that result in regional coexistence
         if tau12 > 0.13 && tau12 < 0.3 && tau21 < 0.15 || tau21 > 0.3
             maxIterations = 2000;
