74ae8ba4d0749cea25e35aa8ac6b4215a9c55671
pjaml <lutzx119@umn.edu>
2023-03-06 02:13:47 -0600

    Added lines to reduce size of arrays before saving

--- tauSweep.m
+++ tauSweep.m
@@ -1,11 +1,15 @@
 iterations = 100;
-maxIterations = 1000;
+maxIterations = 400;
 [tau12Range, tau21Range] = deal(0:0.01:0.4);
 outputDir = './tauSweep/';
 
 for tau12 = tau12Range
 
     for tau21 = tau21Range
+
+        if tau12 > 0.13 && tau12 < 0.3 && tau21 < 0.15 || tau21 > 0.3
+            maxIterations = 1000;
+        end
 
         simMutualism('tau12', tau12, 'tau21', tau21, 'outputDir', outputDir, 'iterations', iterations, 'maxIterations', maxIterations);
 
