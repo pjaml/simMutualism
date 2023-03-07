6e738c038be163fe0c73f040d843ac070b5144a9
lutzx119 <lutzx119@umn.edu>
2023-03-07 08:44:29 -0600

    Increased SBATCH time

--- jobscript.sh
+++ jobscript.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=4:00:00
+#SBATCH --time=18:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lutzx119@umn.edu
 #SBATCH --output=/home/shawa/lutzx119/reports/tausweep-%j.out
