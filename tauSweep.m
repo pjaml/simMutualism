d73aa42f5f5526c4cd13cd0b9b5b9f88eed62ed6
lutzx119 <lutzx119@umn.edu>
2023-03-08 13:56:21 -0600

    no longer setting maxIterations for all sims

--- tauSweep.m
+++ tauSweep.m
@@ -3,8 +3,6 @@
 % values of tau12 and tau21) because it seems parfor requires it
 
 rangeStep = 0.01;
-iterations = 100;
-maxIterations = 1000;
 
 outputDir = '/home/shawa/lutzx119/tauSweep/';
 
@@ -19,10 +17,10 @@ parfor j = 0:40
 
     % more iterations for tau values that result in regional coexistence
     if (tau12 > 0.13 && tau12 < 0.25 && tau21 < 0.15) || (tau21 > 0.28 && tau12 > 0.23 && tau12 < 0.3)
-        maxIterations = 1000;
+        simMutualism(outputDir, 'tau12', tau12, 'tau21', tau21, 'maxIterations', maxIterations, 'diameter', 3600);
+    else
+        simMutualism(outputDir, 'tau12', tau12, 'tau21', tau21);
     end
-
-    simMutualism(outputDir, 'tau12', tau12, 'tau21', tau21, 'maxIterations', maxIterations);
 
 end
 % Sweep script:1 ends here
