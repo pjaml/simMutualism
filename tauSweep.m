ea790926539e3a44aec8fc24c43a104040454598
lutzx119 <lutzx119@umn.edu>
2023-03-08 14:09:25 -0600

    Fixed outputDir now optional parameter

--- tauSweep.m
+++ tauSweep.m
@@ -9,7 +9,7 @@ outputDir = '/home/shawa/lutzx119/tauSweep/';
 % instead of using a for loop for the tau12 values, we can use Slurm to set up
 % jobs for each tau12 value. To change the range of tau12 values, modify the
 % "SBATCH --array=" line in the Slurm job script.
-tau12 = rangeStep * uint16(str2num(getenv("SLURM_ARRAY_TASK_ID")))
+tau12 = rangeStep * str2num(getenv("SLURM_ARRAY_TASK_ID"));
 
 parfor j = 0:40
 
@@ -17,9 +17,9 @@ parfor j = 0:40
 
     % more iterations for tau values that result in regional coexistence
     if (tau12 > 0.13 && tau12 < 0.25 && tau21 < 0.15) || (tau21 > 0.28 && tau12 > 0.23 && tau12 < 0.3)
-        simMutualism(outputDir, 'tau12', tau12, 'tau21', tau21, 'maxIterations', maxIterations, 'diameter', 3600);
+        simMutualism('outputDir', outputDir, 'tau12', tau12, 'tau21', tau21, 'maxIterations', maxIterations, 'diameter', 3600);
     else
-        simMutualism(outputDir, 'tau12', tau12, 'tau21', tau21);
+        simMutualism('outputDir', outputDir, 'tau12', tau12, 'tau21', tau21);
     end
 
 end
