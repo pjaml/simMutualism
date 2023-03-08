8425ad5752fd2c001cfd60ff80b7f4d2edb8d02c
pjaml <lutzx119@umn.edu>
2023-03-07 22:09:54 -0600

    Updated tauSweep

--- tauSweep.m
+++ tauSweep.m
@@ -1,25 +1,19 @@
 % [[file:mutual_ide.org::*Sweep script][Sweep script:1]]
-cl=parcluster('local')
-pool = cl.parpool(16)
-
 % use integers for the number of iterations to run
 % since parfor requires it
-rangeStartInt = 0
-rangeInt = 40
-rangeStep = 0.01
 iterations = 100;
 maxIterations = 1000;
-[tau12Range, tau21Range] = deal(rangeStartInt:rangeStartInt);
 
 outputDir = '~/tauSweep/';
 
 parfor i = tau12Range
 
-    tau12 = i * rangeStep;
 
     for j = tau21Range
 
+        tau12 = i * rangeStep;
         tau21 = j * rangeStep;
+
         % more iterations for tau values that result in regional coexistence
         if tau12 > 0.13 && tau12 < 0.3 && tau21 < 0.15 || tau21 > 0.3
             maxIterations = 2000;
