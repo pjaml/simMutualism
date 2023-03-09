a098e8dcff6f2fa4eee11f2efedf9ab22865a614
lutzx119 <lutzx119@umn.edu>
2023-03-08 19:51:35 -0600

    Changed jobscript time to 1 hour

--- jobscript.sh
+++ jobscript.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8G
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 #SBATCH --array=0-40
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lutzx119@umn.edu
