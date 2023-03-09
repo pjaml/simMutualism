7926381ba18ad1ea9918e4d2d25f0c6fccf1a15e
lutzx119 <lutzx119@umn.edu>
2023-03-09 14:27:55 -0600

    fix find target filename in generatePlots.m

--- generatePlots.m
+++ generatePlots.m
@@ -6,8 +6,13 @@ formatSpec = '%.2f';
 % tau12 and tau21 pairs
 taus = [0 0; 0.05 0; 0.05 0.05; 0.05 0.10; 0.15 0.05; 0.20 0.05; 0.20 0.15; 0.25 0.05; 0.23 0.37; 0.26 0.37; 0.35 0.37];
 
+
 for i = 1:length(taus)
-    filename = strcat(sweepDir, "results_tau12_", num2str(taus(i, 1), formatSpec), "_tau21_", num2str(taus(i, 2), formatSpec), ".mat");
+
+    % probably a better way to do this with regexp
+    targetFile = dir(strcat(sweepDir, "*tau12_", num2str(taus(i, 1), formatSpec), "*tau21_", num2str(taus(i, 2), formatSpec), "*.mat"));
+
+    filename = targetFile.name;
 
     plotPopSpaceTime(filename, 'figDir', figDir);
 
