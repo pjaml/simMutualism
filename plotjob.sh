19bc5d8f35fb9d6dd334c59e582d14f63e6185ab
lutzx119 <lutzx119@umn.edu>
2023-03-09 18:24:27 -0600

    nit lower time and more memory for plotjob

--- plotjob.sh
+++ plotjob.sh
@@ -2,7 +2,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=3:00:00
+#SBATCH --mem=32G
+#SBATCH --time=1:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lutzx119@umn.edu
 #SBATCH --output=/home/shawa/lutzx119/reports/plotjob-%j.out
