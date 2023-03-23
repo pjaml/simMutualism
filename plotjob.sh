cdf8f55f1e436402ea01bcf80aa819fe1df553f1
lutzx119 <lutzx119@umn.edu>
2023-03-23 01:24:24 -0500

    nit: run plotjob.sh for 6 hours max

--- plotjob.sh
+++ plotjob.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lutzx119@umn.edu
 #SBATCH --output=/home/shawa/lutzx119/reports/plotjob-%j.out
