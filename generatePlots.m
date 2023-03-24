56bb4b76d5e53cefc60451af2648040c12cb51e3
lutzx119 <lutzx119@umn.edu>
2023-03-24 00:31:24 -0500

    fix: only loop through generatePlots when nec.

--- generatePlots.m
+++ generatePlots.m
@@ -3,7 +3,7 @@ function generatePlots(sweepDir, figDir, varargin)
 
     p = inputParser;
     addRequired(p, 'sweepDir', @isfolder);
-    addRequired(p, 'figDir', @isfolder);
+    addRequired(p, 'figDir');
     addParameter(p, 'plotOutcomes', false, @islogical);
     addParameter(p, 'plotPopSpaceTime', false, @islogical);
     addParameter(p, 'plotFinalPopSpace', false, @islogical);
@@ -13,9 +13,6 @@ function generatePlots(sweepDir, figDir, varargin)
 
     parse(p, sweepDir, figDir, varargin{:});
 
-    tau12Range = p.Results.tau12Range;
-    tau21Range = p.Results.tau21Range;
-
     mkdir(figDir)
 
     if p.Results.plotOutcomes
@@ -24,38 +21,44 @@ function generatePlots(sweepDir, figDir, varargin)
         plotOutcomes(sweepDir, 'figDir', figDir);
     end
 
-    formatSpec = '%.2f';
+    if p.Results.plotPopSpaceTime || p.Results.plotFinalPopSpace || p.Results.plotSpeedTime
 
-    taus = [];
+        tau12Range = p.Results.tau12Range;
+        tau21Range = p.Results.tau21Range;
 
-    for tau12 = tau12Range
+        taus = [];
 
-        taus = [taus; ones(numel(tau21Range), 1) * tau12, tau21Range(:)];
+        for tau12 = tau12Range
 
-    end
+            taus = [taus; ones(numel(tau21Range), 1) * tau12, tau21Range(:)];
 
-    for i = 1:length(taus)
-
-        % probably a better way to do this with regexp
-        targetFile = dir(fullfile(sweepDir, strcat("*tau12_", num2str(taus(i, 1), formatSpec), "*tau21_", num2str(taus(i, 2), formatSpec), "*.mat")));
-
-        filename = fullfile(sweepDir, targetFile.name);
-
-        curFile = load(filename, 'iterations', 'filename', 'nP', 'nF1', 'nF2', 'nThreshold', 'x', 'instantSpeedP', 'instantSpeedF1', 'instantSpeedF2');
-
-        if p.Results.plotPopSpaceTime
-            plotPopSpaceTime(curFile, 'figDir', figDir);
         end
 
-        if p.Results.plotFinalPopSpace
-            plotFinalPopSpace(curFile, 'figDir', figDir);
-        end
+        for i = 1:length(taus)
 
-        if p.Results.plotSpeedTime
-            plotSpeedTime(curFile, 'figDir', figDir);
-        end
+            formatSpec = '%.2f';
 
-        clear curFile;
+            % probably a better way to do this with regexp
+            targetFile = dir(fullfile(sweepDir, strcat("*tau12_", num2str(taus(i, 1), formatSpec), "*tau21_", num2str(taus(i, 2), formatSpec), "*.mat")));
+
+            filename = fullfile(sweepDir, targetFile.name);
+
+            curFile = load(filename, 'iterations', 'filename', 'nP', 'nF1', 'nF2', 'nThreshold', 'x', 'instantSpeedP', 'instantSpeedF1', 'instantSpeedF2');
+
+            if p.Results.plotPopSpaceTime
+                plotPopSpaceTime(curFile, 'figDir', figDir);
+            end
+
+            if p.Results.plotFinalPopSpace
+                plotFinalPopSpace(curFile, 'figDir', figDir);
+            end
+
+            if p.Results.plotSpeedTime
+                plotSpeedTime(curFile, 'figDir', figDir);
+            end
+
+            clear curFile;
+        end
     end
 end
 % Generate figures from paper:1 ends here
